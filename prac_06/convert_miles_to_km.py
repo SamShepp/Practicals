017aeefa3342f95512b66dd19309c42e62e5b063
jc463652 <sam_shepp@hotmail.com>
2017-09-20 13:01:56 +1000

    add convert miles to km prac and squaring prac

---
+++ prac_06/convert_miles_to_km.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class ConvertMilesToKm(App):
+    def build(self):
+        Window.size = (400, 200)
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_to_km.kv')
+        return self.root
+
+    def handle_conversion(self):
+        result = self.get_validated_miles() * 1.609344
+        self.root.ids.display_label.text = "{:.3f}".format(result)
+
+    def get_validated_miles(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_increment(self, increment):
+            result = self.get_validated_miles() + increment
+            self.root.ids.input_number.text = str(result)
+
+
+ConvertMilesToKm().run()
